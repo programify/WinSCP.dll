3c5fa33db7e68e0991dc7d8ecbf1c5438df28dba
Clive Hudson <github@programify.com>
2020-01-26 10:23:48 +0000

    Add readme

---
+++ _ReadMe.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinSCP
+{
+class _ReadMe
+{
+}
+}
+
+/*
+ *   This C# Windows Library solution includes only the DLL assembly 
+ *   WinSCP.dll (alias: "winscpnet.dll") using source code originating
+ *   from WinSCP[1].
+ *
+ *   Another required assembly is the WinSCP executable "winscp.exe".
+ *   The version numbers of both assemblies must match.
+ */
+
+/*
+ *   References:
+ *
+ *   [1] https://winscp.net/eng/index.php
+ */
